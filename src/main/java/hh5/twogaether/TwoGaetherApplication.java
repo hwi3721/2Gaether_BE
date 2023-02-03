c39982b800e72b4c41df5d6322a83368b76544cf
hwi3721 <lsh3721@gmail.com>
2023-02-04 00:02:02 +0900

    first commit - directory set

---
+++ src/main/java/hh5/twogaether/TwoGaetherApplication.java
@@ -0,0 +1,13 @@
+package hh5.twogaether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoGaetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwoGaetherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hh5/twogaether/TwoGaetherApplicationTests.java
@@ -0,0 +1,13 @@
+package hh5.twogaether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoGaetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
