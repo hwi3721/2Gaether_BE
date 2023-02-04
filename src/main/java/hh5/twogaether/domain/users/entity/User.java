048f3ff783657b59184b2f990bf2baf2593d98c3
hwi3721 <lsh3721@gmail.com>
2023-02-04 22:04:13 +0900

    연습

--- src/main/java/hh5/twogaether/domain/users/entity/User.java
+++ src/main/java/hh5/twogaether/domain/users/entity/User.java
@@ -1,7 +1,8 @@
 package hh5.twogaether.domain.users.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-
+@AllArgsConstructor
 @Getter
 public class User {
     private UserRoleEnum role;
