fe0ebae571832b10cf556330e1c77bd3e6c4232c
midcondria <hyukkind@naver.com>
2023-02-06 22:02:48 +0900

    #8 헬스체크용 페이지 url "/"인 페이지 제작

--- src/main/java/hh5/twogaether/domain/users/controller/UserController.java
+++ src/main/java/hh5/twogaether/domain/users/controller/UserController.java
@@ -1,4 +1,29 @@
 package hh5.twogaether.domain.users.controller;
 
+import hh5.twogaether.domain.users.dto.UserSignupRequestDto;
+import hh5.twogaether.domain.users.service.UserService;
+import hh5.twogaether.security.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
+
+    @GetMapping
+    public String healthCheck() {
+        return "hello";
+    }
+
+//    @PostMapping("/users/signup")
+//    public String createUser(UserSignupRequestDto signupRequestDto) {
+//        userService.checkEmailDuplication(signupRequestDto.getEmail());
+//    }
 }
