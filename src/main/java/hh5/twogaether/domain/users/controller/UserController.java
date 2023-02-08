ce104e3df0f524c7231af20f77fba550d3d82327
midcondria <hyukkind@naver.com>
2023-02-08 10:22:09 +0900

    #26 회원가입 기능 컨트롤러 및 서비스

--- src/main/java/hh5/twogaether/domain/users/controller/UserController.java
+++ src/main/java/hh5/twogaether/domain/users/controller/UserController.java
@@ -1,18 +1,30 @@
 package hh5.twogaether.domain.users.controller;
 
+import hh5.twogaether.domain.users.dto.LoginRequestDto;
+import hh5.twogaether.domain.users.dto.LoginResponseDto;
+import hh5.twogaether.domain.users.dto.SignUpRequestDto;
+import hh5.twogaether.domain.users.entity.User;
+import hh5.twogaether.domain.users.service.UserService;
 import hh5.twogaether.security.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static hh5.twogaether.security.jwt.JwtUtil.AUTHORIZATION_HEADER;
+import static org.springframework.http.HttpStatus.OK;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
-//    private final UserService userService;
+    private final UserService userService;
     private final JwtUtil jwtUtil;
 
     @GetMapping
@@ -20,8 +32,22 @@ public class UserController {
         return "hello";
     }
 
-//    @PostMapping("/users/signup")
-//    public String createUser(UserSignupRequestDto signupRequestDto) {
-//        userService.checkEmailDuplication(signupRequestDto.getEmail());
-//    }
+    //회원 가입
+    @PostMapping("/users/signup")
+    public String signUp(@RequestBody SignUpRequestDto signupRequestDto) {
+        userService.checkEmailDuplication(signupRequestDto.getEmail());
+        userService.createUser(signupRequestDto);
+
+        return "ok";
+    }
+
+    @PostMapping("/users/login")
+    public ResponseEntity<LoginResponseDto> loginUser(@RequestBody LoginRequestDto loginRequestDto,
+                                                      HttpServletResponse response) {
+
+        User user = userService.login(loginRequestDto);
+        response.addHeader(AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername()));
+
+        return new ResponseEntity<>(new LoginResponseDto(OK.value(), "로그인 완료", user.getUsername()),OK);
+    }
 }
--- src/main/java/hh5/twogaether/domain/users/service/UserService.java
+++ src/main/java/hh5/twogaether/domain/users/service/UserService.java
@@ -1,9 +1,12 @@
 package hh5.twogaether.domain.users.service;
 
+import hh5.twogaether.domain.users.dto.LoginRequestDto;
+import hh5.twogaether.domain.users.dto.SignUpRequestDto;
 import hh5.twogaether.domain.users.entity.User;
 import hh5.twogaether.domain.users.repository.UserRepository;
-import hh5.twogaether.exception.message.ExceptionMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,19 +19,32 @@ import static hh5.twogaether.exception.message.ExceptionMessage.*;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public void checkEmailDuplication(String email) {
-//        String findEmail = userRepository.findByUsername(email);
-
-//        if (isExisted(findEmail)) {
-//            throw new IllegalArgumentException(EXISTED_EMAIL.getDescription());
-//        }
+    @Transactional
+    public void createUser(SignUpRequestDto signupRequestDto) {
+        String encryptPassword = passwordEncoder.encode(signupRequestDto.getPassword());
+        SignUpRequestDto encryptSignUpRequestDto = new SignUpRequestDto(signupRequestDto, encryptPassword);
+        User user = new User(encryptSignUpRequestDto);
+        userRepository.save(user);
     }
 
-//    @Transactional
-//    public String
+    public User login(LoginRequestDto loginRequestDto) {
+        User users = userRepository.findByUsername(loginRequestDto.getEmail())
+                .orElseThrow(() -> new BadCredentialsException(INCORRECT_SIGN_IN_TRY.getDescription()));
 
-    private boolean isExisted(String field) {
-        return field != null;
+        if (!passwordEncoder.matches(loginRequestDto.getPassword(), users.getPassword())) {
+            throw new BadCredentialsException(INCORRECT_SIGN_IN_TRY.getDescription());
+        }
+
+        return users;
+    }
+
+    public void checkEmailDuplication(String email) {
+        Optional<User> foundUser = userRepository.findByUsername(email);
+
+        if (foundUser.isPresent()) {
+            throw new IllegalArgumentException(EXISTED_EMAIL.getDescription());
+        }
     }
 }
