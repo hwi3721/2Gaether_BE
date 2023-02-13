b9c9b6ce9be1c1ecf95948812067b931b02b8730
lcoeda <120078825+lcoeda@users.noreply.github.com>
2023-02-13 16:09:15 +0900

    #46 소켓 스톰프 초안
    stomp 뼈대 잡아놨습니다.
    /chat/room으로 가면 테스트해볼 수 있습니다.

---
+++ src/main/java/hh5/twogaether/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package hh5.twogaether.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/sub");
+        registry.enableSimpleBroker("/pub");   // Enables a simple in-memory broker
+    }
+}
