34074509b4f67bfa7d0f56e2f759e7103cb0c1d7
midcondria <hyukkind@naver.com>
2023-02-15 15:21:47 +0900

    #78 Dto 추가 Dog 추가 DTO 및 이미지 응답 DTO 추가

--- src/main/java/hh5/twogaether/domain/dog/dto/DogSignupRequestDto.java
+++ src/main/java/hh5/twogaether/domain/dog/dto/DogSignupRequestDto.java
@@ -1,6 +1,9 @@
 package hh5.twogaether.domain.dog.dto;
 
 import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Getter
 public class DogSignupRequestDto {
@@ -8,5 +11,12 @@ public class DogSignupRequestDto {
     private String dogName;
     private String dogSex;
     private String dogDetails;
+    private List<MultipartFile> images;
 
+    public DogSignupRequestDto(String dogName, String dogSex, String dogDetails, List<MultipartFile> images) {
+        this.dogName = dogName;
+        this.dogSex = dogSex;
+        this.dogDetails = dogDetails;
+        this.images = images;
+    }
 }
