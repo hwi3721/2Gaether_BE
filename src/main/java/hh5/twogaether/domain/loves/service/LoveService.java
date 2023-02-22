fc10b0bd06f7ff28050d176d5b8262f88870c8bc
midcondria <hyukkind@naver.com>
2023-02-22 15:06:21 +0900

    #91 LoveService MatchController 와 연동하여 받은 좋아요 목록에서의 수락 기능과 MatchController 에서의 좋아요를 같은 메서드로 묶어 상태코드로 표현 0은 한쪽만 좋아요 / 1은 양쪽이 서로 좋아요 / 2는 거절

---
+++ src/main/java/hh5/twogaether/domain/loves/service/LoveService.java
@@ -0,0 +1,59 @@
+package hh5.twogaether.domain.loves.service;
+
+import hh5.twogaether.domain.dog.entity.Dog;
+import hh5.twogaether.domain.dog.repository.DogRepository;
+import hh5.twogaether.domain.loves.dto.LoveReceivedDto;
+import hh5.twogaether.domain.loves.dto.LoveSentDto;
+import hh5.twogaether.domain.loves.entity.Love;
+import hh5.twogaether.domain.loves.repository.LoveRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class LoveService {
+
+    private final LoveRepository loveRepository;
+    private final DogRepository dogRepository;
+
+    @Transactional
+    public void loveUser(Long opponentId, Long myId) {
+        Love acceptCase = loveRepository.findByOpponentIdAndCreatedBy(myId, opponentId);
+        Love sendCase = loveRepository.findByOpponentIdAndCreatedBy(opponentId, myId);
+        if (sendCase == null && acceptCase == null) {
+            loveRepository.save(new Love(opponentId));
+        } else {
+            acceptCase.accept();
+        }
+    }
+
+    @Transactional
+    public void rejectLove(Long opponentId, Long myId) {
+        Love foundLove = loveRepository.findByOpponentIdAndCreatedBy(myId, opponentId);
+        foundLove.reject();
+    }
+
+    //보낸 좋아요 조회
+    public List<LoveSentDto> getSentLove(Long id) {
+        Dog foundDog = dogRepository.findByCreatedBy(id).get(0);
+        List<LoveSentDto> loving = loveRepository.findAllNotAcceptedByCreatedBy(id).stream()
+                .map(love -> new LoveSentDto(love, foundDog))
+                .collect(Collectors.toList());
+        return loving;
+    }
+
+    //받은 좋아요 조회
+    public List<LoveReceivedDto> getReceivedLove(Long id) {
+        Dog foundDog = dogRepository.findByCreatedBy(id).get(0);
+        List<LoveReceivedDto> loved = loveRepository.findAllNotAcceptedByOpponentId(id).stream()
+                .map(love -> new LoveReceivedDto(love, foundDog))
+                .collect(Collectors.toList());
+        return loved;
+    }
+
+
+}
