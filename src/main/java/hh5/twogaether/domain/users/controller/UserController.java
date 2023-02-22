426bb31d4550efa8e9b4fb9c8063c3e4bd344837
midcondria <hyukkind@naver.com>
2023-02-22 15:21:36 +0900

    #93 탐색 범위 지정 field 추가 필요시 range 변경으로 탐색 범위 설정 가능

--- src/main/java/hh5/twogaether/domain/users/controller/UserController.java
+++ src/main/java/hh5/twogaether/domain/users/controller/UserController.java
@@ -10,10 +10,7 @@ import hh5.twogaether.security.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -50,7 +47,7 @@ public class UserController {
         User user = userService.login(loginRequestDto);
         response.addHeader(AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername()));
 
-        return new ResponseEntity<>(new LoginResponseDto(OK.value(), "로그인 완료", user.getUsername()),OK);
+        return new ResponseEntity<>(new LoginResponseDto(OK.value(), "로그인 완료", user.getUsername()), OK);
     }
 
     @PostMapping("/users/dupcheck")
@@ -58,5 +55,4 @@ public class UserController {
         userService.checkEmailDuplication(signUpRequestDto.getEmail());
         return new ResponseEntity<>(new ResponseMessageDto(OK.value(), "사용 가능한 이메일입니다."), OK);
     }
-
 }
