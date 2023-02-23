a428a3fa5d3ce8d90595addfdd3a659dd41d36e3
midcondria <hyukkind@naver.com>
2023-02-23 14:03:19 +0900

    #85 MatchController 변수명 변경 받아야 할 값이 opponentId -> dogId로 변경되어 직관성을 위해 수정함

--- src/main/java/hh5/twogaether/domain/match/controller/MatchController.java
+++ src/main/java/hh5/twogaether/domain/match/controller/MatchController.java
@@ -1,7 +1,7 @@
 package hh5.twogaether.domain.match.controller;
 
 import hh5.twogaether.domain.loves.service.LoveService;
-import hh5.twogaether.domain.match.dto.MatchResponseDto;
+import hh5.twogaether.domain.match.dto.MatchDogResponseDto;
 import hh5.twogaether.domain.match.service.MatchService;
 import hh5.twogaether.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
@@ -12,30 +12,31 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/match")
 public class MatchController {
+
     private final MatchService matchService;
     private final LoveService loveService;
 
     //매칭 상대 보기
     @GetMapping
-    public MatchResponseDto showMatches(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public MatchDogResponseDto showMatches(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return matchService.getMatches(userDetails.getUser().getId());
     }
 
     //좋아요
-    @GetMapping("/love/{opponentId}")
-    public MatchResponseDto loveAndShowNext(@AuthenticationPrincipal UserDetailsImpl userDetails,
-                                            @PathVariable Long opponentId) {
-        //좋아요 로직//
-        loveService.loveUser(opponentId, userDetails.getUser().getId());
-        matchService.passUser(opponentId, userDetails.getUser().getId());
+    @GetMapping("/love/{dogId}")
+    public MatchDogResponseDto loveAndShowNext(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                               @PathVariable Long dogId) {
+        //좋아요 로직
+        loveService.loveUser(dogId, userDetails.getUser().getId());
+        matchService.passUser(dogId, userDetails.getUser().getId());
         return matchService.getMatches(userDetails.getUser().getId());
     }
 
     //싫어요
-    @GetMapping("/reject/{opponentId}")
-    public MatchResponseDto rejectAndShowNext(@AuthenticationPrincipal UserDetailsImpl userDetails,
-                                     @PathVariable Long opponentId) {
-        matchService.passUser(opponentId,userDetails.getUser().getId());
+    @GetMapping("/reject/{dogId}")
+    public MatchDogResponseDto rejectAndShowNext(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                     @PathVariable Long dogId) {
+        matchService.passUser(dogId,userDetails.getUser().getId());
         return matchService.getMatches(userDetails.getUser().getId());
     }
 }
