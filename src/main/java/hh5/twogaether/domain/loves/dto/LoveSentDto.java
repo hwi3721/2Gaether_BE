323288562ab0e5173ccabb2d7392da4b929ddfb5
midcondria <hyukkind@naver.com>
2023-03-11 03:27:29 +0900

    #91 이미지 없을 시 기본 이미지 처리

--- src/main/java/hh5/twogaether/domain/loves/dto/LoveSentDto.java
+++ src/main/java/hh5/twogaether/domain/loves/dto/LoveSentDto.java
@@ -1,6 +1,5 @@
 package hh5.twogaether.domain.loves.dto;
 
-import hh5.twogaether.domain.dog.entity.Dog;
 import hh5.twogaether.domain.loves.entity.Love;
 import lombok.Getter;
 
@@ -23,9 +22,11 @@ public class LoveSentDto {
         this.userId = love.getOpponent().getId();
         this.dogId = love.getOpponent().getDogs().get(0).getId();
         this.dogName = love.getOpponent().getDogs().stream()
-                .filter(dog -> dog.isDelete() == false)
+                .filter(dog -> !dog.isDelete())
                 .collect(Collectors.toList()).get(0).getDogName();
         this.dogSex = love.getOpponent().getDogs().get(0).getDogSex();
-        this.imageUrl = love.getOpponent().getDogs().get(0).getDogImages().get(0).getImgUrl();
+        this.imageUrl = love.getOpponent().getDogs().get(0).getDogImages().isEmpty()
+                ? "https://midcon.s3.ap-northeast-2.amazonaws.com/be7648fa-5b71-47c4-8b9f-a1defc6a28cc.jpg"
+                : love.getOpponent().getDogs().get(0).getDogImages().get(0).getImgUrl();
     }
 }
