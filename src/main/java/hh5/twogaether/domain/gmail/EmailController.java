8868a776ac0f9e84bb351633f530609519226db1
hwi3721 <100186336+hwi3721@users.noreply.github.com>
2023-03-15 13:10:28 +0900

    Update EmailController.java
    진짜 되나?

--- src/main/java/hh5/twogaether/domain/gmail/EmailController.java
+++ src/main/java/hh5/twogaether/domain/gmail/EmailController.java
@@ -20,7 +20,7 @@ public class EmailController {
 
         EmailCheckSuccessResponseDto emailCheckSuccessResponseDto = new EmailCheckSuccessResponseDto(200, ResponseMessage.EMAIL_SUCCESS_CHECK_LINK);
 //        return new ResponseEntity<>(emailCheckSuccessResponseDto, HttpStatus.OK);
-        return redirect:/email/email.html;
+        return "/email";
     }
 
     //@GetMapping("/pwreset/{emailCode}/{userEmail}")
